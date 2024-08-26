e31c3b800e9b0fccc33c05ea1e5ba5a4b75b8b39
Ishan Surana <121395061+ishan-surana@users.noreply.github.com>
2024-08-26 10:10:19 +0530

    Upload IS Lab 5

--- IS Lab 5/1.py
+++ IS Lab 5/1.py
@@ -1,1 +1,12 @@
+def hash_function(s):
+    hash_value = 5381
+    current_xor = 17 # 5+3+8+1 = 17
+    for char in s:
+        hash_value = (hash_value * 33) + ord(char)
+        hash_value = (hash_value ^ current_xor) << 4
+        current_xor = ord(char)
+    hash_value = hash_value & 0xFFFFFFFF
+    return hash_value
 
+input_string = "hello"
+print(f"Hash value: {hash_function(input_string)}")
